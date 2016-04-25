2fb08e3e000510321086165b359d32900a5d7d7f
gnkoua <gnkoua@hotmail.com>
2016-04-25 10:53:14 -0500

    I delete hello world message

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,7 +5,7 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("");
         }
     }
 }
